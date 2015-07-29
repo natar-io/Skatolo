c60940cc567920fcb85c6c73e59a8e6a2262bffa
Jeremy Laviole <jeremy.laviole@inria.fr>
2015-07-29 11:58:47 +0200

    Ruby linker updated.

--- examples/ruby/cp5.rb
+++ examples/ruby/cp5.rb
@@ -23,8 +23,10 @@ end
 
 class ControlP5 < Java::FrInriaControlP5::ControlP5
 
-  def initialize (applet)
+  def initialize (applet, events_object = nil)
     @event_handler = EventHandler.new self
+    @events_object = events_object if events_object != nil
+    @events_object = applet if events_object == nil
 
     super(applet, @event_handler)
     @applet = applet
@@ -36,7 +38,7 @@ class ControlP5 < Java::FrInriaControlP5::ControlP5
       name = controller.name
       ## There is a method with this name... 
 
-      if not (@applet.respond_to? name + "_value")
+      if not (@events_object.respond_to? name + "_value")
         #        puts "please declare a method for " + name
         create_getter_for name
         create_setter_for name
@@ -50,19 +52,29 @@ class ControlP5 < Java::FrInriaControlP5::ControlP5
     controller = get(controlEvent.getName)
     name = controlEvent.getName
     value = controlEvent.getValue
+    string_value = controlEvent.getStringValue
     
     ## There is a method with this name... 
-    if @applet.respond_to? name
+    if @events_object.respond_to? name
 
       ## Buttons usually, not arguments. 
-      @applet.send(name)  if is_event_class controller.class
+      @events_object.send(name)  if is_event_class controller.class
 
       ## Sliders, check arity 
       if is_value_class controller.class
 
         ## try to send the value 
-        @applet.send(name, value)
+        @events_object.send(name, value)
       end 
+
+      ## Text 
+      ## Sliders, check arity 
+      if is_string_value_class controller.class
+        ## try to send the value 
+        @events_object.send(name, string_value)
+      end 
+
+
     end
 
   end
@@ -76,7 +88,7 @@ class ControlP5 < Java::FrInriaControlP5::ControlP5
 
 #    puts "Creating a getter for " + name
 
-    @applet.create_method(name + "_value") do 
+    @events_object.create_method(name + "_value") do 
       controller = @cp5.get(name)
       @cp5.get_controller_value controller
     end
@@ -91,7 +103,7 @@ class ControlP5 < Java::FrInriaControlP5::ControlP5
     value = get_controller_value controller
 #    puts "Creating a setter for " + name
     
-    @applet.create_method(name + "_value=") do |value|
+    @events_object.create_method(name + "_value=") do |value|
       controller = @cp5.get(name)
       @cp5.set_controller_value controller, value
     end
@@ -113,18 +125,16 @@ class ControlP5 < Java::FrInriaControlP5::ControlP5
 
   
   def is_event_class object_class
-    object_class == Java::FrInriaControlP5GuiControllers::Button
-
+    object_class == Java::FrInriaControlP5GuiControllers::Button or 
+      object_class == Java::FrInriaControlP5GuiControllers::Bang
   end
 
   def is_value_class object_class
     object_class == Java::FrInriaControlP5GuiControllers::Slider
   end
   
-## TODO
   def is_string_value_class object_class
-    False
-    #    object_class == Java::FrInriaControlP5GuiControllers::Slider
+    object_class == Java::FrInriaControlP5GuiControllers::Textfield
   end
   
 
