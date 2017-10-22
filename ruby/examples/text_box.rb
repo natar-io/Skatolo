1fdcc45e41445fd1895a6ddeb6492a7d84404d07
monkstone <mamba2928@yahoo.co.uk>
2017-10-22 14:07:29 +0100

    Sort of Working Text Field

--- ruby/examples/text_box.rb
+++ ruby/examples/text_box.rb
@@ -2,7 +2,7 @@ require 'skatolo'
 # In this simple sketch we attach two buttons to skatolo in the regular way,
 # named buttons 'press_me' and 'reset' thanks to some fancy metaprogramming
 # we can create methods :press_me and :reset for the buttons
-attr_reader :skatolo, :back_color
+attr_reader :skatolo, :back_color, :font
 
 def settings
   size(400, 300)
@@ -15,12 +15,12 @@ def setup
 	       .set_position(10, 10)
          .set_size(50, 20)
 	       .set_label('Press Me!')
-  skatolo.add_textfield('input_name')
+  @input = skatolo.add_textfield('input_name')
 	       .set_position(10, 50)
          .set_size(100, 20)
-         .set_focus(true)
    skatolo.update # this step is important
    @back_color = color(200, 0, 200)
+   text_font(create_font('monospace', 30))
 end
 
 def create_method(name, &block)
@@ -29,8 +29,8 @@ end
 
 def draw
   background(back_color)
-end
-
-def press_me
-  puts format("Hello %s", input_name_value.to_s)
+  if input_name_value.length > 0
+    text("Hello #{input_name_value}", 10,120)
+    no_loop
+  end
 end
